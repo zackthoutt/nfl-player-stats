828054b7e6ca1e5442741fa4e5c8bca2753c8c54
Zack Thoutt <zynicide@gmail.com>
2017-11-27 12:05:54 -0700

    feat: create scraper class and start building for multi-processing

--- scrape-nfl-stats.py
+++ scrape-nfl-stats.py
@@ -1,4 +1,40 @@
+import requests
+from multiprocessing.dummy import Pool
+import time
+
 
 PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
 PLAYER_OVERVIEW_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
 PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
+
+
+class Scraper():
+    """Scraper for pro-football-reference.com to collect NFL player stats"""
+
+    def __init__(self, letters_to_scrape=['A'], num_jobs=1, clear_old_data=True):
+        """Initialize the scraper to get player stats
+
+                Args:
+                    - letters_to_scrape (str[]): The site sorts players by the first letter of their
+                      last name. This array tells the scraper which letters to scrape data for.
+                    - num_jobs (int): Number of concurrent jobs the scraper should run. While Python
+                      can't multi-thread, it can manage multiple processes at once, which allows it to
+                      utilize time spent waiting for the server to respond.
+                    - clear_old_data (boolean): Whether or not the data file should be wiped before
+                      starting the scrape.
+
+                Returns:
+                    None
+        """
+        self.letters_to_scrape = [letter.upper() for letter in letters_to_scrape]
+        self.num_jobs = num_jobs
+        self.clear_old_data = clear_old_data
+        self.session = requests.Session()
+        self.start_time = time.time()
+        self.cross_process_player_count = 0
+
+        if num_jobs > 1:
+            self.multiprocessing = True
+            self.worker_pool = Pool(num_jobs)
+        else:
+            self.multiprocessing = False
