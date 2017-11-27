ada527c719cde56668aea5680c9e1135039b0d29
Zack Thoutt <zynicide@gmail.com>
2017-11-27 11:55:01 -0700

    feat: add base pages to use scraping

---
+++ scrape-nfl-stats.py
@@ -0,0 +1,4 @@
+
+PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
+PLAYER_OVERVIEW_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
+PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
