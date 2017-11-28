fcb80a30ee7935ef02b924ecee5fb8c88637cf0f
Zack Thoutt <zackarey.thoutt@colorado.edu>
2017-11-27 21:22:33 -0700

    feat: get list of players by letter

--- scrape-nfl-stats.py
+++ scrape-nfl-stats.py
@@ -1,12 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool
 import time
+import shutil
 
 
 PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
-PLAYER_OVERVIEW_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
+PLAYER_PROFILE_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
 PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
 
+HEADERS = {
+    'user-agent': ('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                   '(KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36')
+}
+
+DATA_DIR = 'data'
 
 class Scraper():
     """Scraper for pro-football-reference.com to collect NFL player stats"""
@@ -38,3 +46,61 @@ class Scraper():
             self.worker_pool = Pool(num_jobs)
         else:
             self.multiprocessing = False
+
+    def scrape_site(self):
+        """Pool workers to scrape players by first letter of last name"""
+        if self.clear_old_data:
+            self.clear_data_dir()
+        for letter in self.letters_to_scrape:
+            player_profile_links = self.get_players_for_letter(letter)
+            print player_profile_links
+
+    def get_players_for_letter(self, letter):
+        """Get a list of player links for a letter of the alphabet.
+            Site organizes players by first letter of last name.
+
+            Args:
+                - letter (str): letter of the alphabet uppercased
+
+            Returns:
+                - player_links (str[]): the URLs to get player profiles
+        """
+        response = self.get_page(PLAYER_LIST_URL.format(letter))
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        players = soup.find('div', {'id': 'div_players'}).find_all('a')
+        return [player['href'] for player in players]
+
+    def get_page(self, url, retry_count=0):
+        """Use requests to get a page; retry when failures occur
+
+            Args:
+                - url (str): The URL of the page to make a GET request to
+                - retry_count (int): Number of times the URL has already been requests
+
+            Returns:
+                - response (obj): The Requests response object
+        """
+        try:
+            return self.session.get(url, headers=HEADERS)
+        except:
+            retry_count += 1
+            if retry_count <= 3:
+                self.session = requests.Session()
+                return self.get_page(url, retry_count)
+            else:
+                raise
+
+    def clear_data_dir(self):
+        """Clears the scraped data"""
+        try:
+            shutil.rmtree(DATA_DIR)
+        except FileNotFoundError:
+            pass
+
+
+if __name__ == '__main__':
+    letters_to_scrape = ['A']
+    nfl_scraper = Scraper(letters_to_scrape=letters_to_scrape, num_jobs=1, clear_old_data=False)
+
+    nfl_scraper.scrape_site()
