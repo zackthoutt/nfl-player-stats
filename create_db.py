ba71460d50236c0b6de42380486e2607fe19eda2
Liam Hawkins <liamhawkinscu@gmail.com>
2017-12-12 20:52:27 -0500

    Refactor create_db.py as a class NFLDatabaseConverter
    Initial refactoring of create_db.py into a class. NFLDatabaseConverter is initialized with database file name db_file and has method create_table_from_json() which takes a table name, and json_file. Still hardcoded to match current 'profiles' json file.

--- create_db.py
+++ create_db.py
@@ -3,50 +3,56 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Numeric, String
 import pandas as pd
 
-PROFILES_JSON = 'profiles_1512362725.022629.json'
-PROFILES_TABLE_NAME = 'profiles'
-PROFILES_DB_FILE= 'nfl-player-stats.db'
 
-engine = create_engine('sqlite:///{}'.format(PROFILES_DB_FILE), echo=True)
-Base = declarative_base()
+class NFLDatabaseConverter():
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.engine = create_engine('sqlite:///{}'.format(db_file), echo=True)
+
+    def create_table_from_json(self, name, json_file):
+        Base = declarative_base()
+        global table_name
+        table_name = name
+
+        class Profile(Base):
+            __tablename__ = table_name
+
+            player_id = Column(Integer, primary_key=True)
+            name = Column(String(50))
+            position = Column(String(2))
+            height = Column(String(4))
+            weight = Column(Integer)
+            current_team = Column(String(3))
+            birth_date = Column(String(10))
+            birth_place = Column(String(30))
+            death_date = Column(String(10))
+            college = Column(String(30))
+            high_school = Column(String(30))
+            draft_team = Column(String(3))
+            draft_round = Column(Integer)
+            draft_position = Column(Integer)
+            draft_year = Column(Integer)
+            current_salary = Column(Integer)
+            hof_induction_year = Column(Integer)
+
+            def __str__(self):
+                output = ''
+                for c in self.__table__.columns:
+                    output += '{}: {}\n'.format(c.name, getattr(self, c.name))
+                return output
+
+        Base.metadata.create_all(self.engine)
+        self.profiles_df = pd.read_json(json_file)
+        self.profiles_df.set_index('player_id', inplace=True)
+        self.profiles_df.to_sql(name=name, con=self.engine, if_exists='replace')
 
 
-class Profile(Base):
-    __tablename__ = PROFILES_TABLE_NAME
+if __name__ == '__main__':
+    DATABASE_FILE = 'nfl-player-stats.db'
+    PROFILES_JSON = 'profiles_1512362725.022629.json'
+    PROFILES_TABLE_NAME = 'profiles'
 
-    player_id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    position = Column(String(2))
-    height = Column(String(4))
-    weight = Column(Integer)
-    current_team = Column(String(3))
-    birth_date = Column(String(10))
-    birth_place = Column(String(30))
-    death_date = Column(String(10))
-    college = Column(String(30))
-    high_school = Column(String(30))
-    draft_team = Column(String(3))
-    draft_round = Column(Integer)
-    draft_position = Column(Integer)
-    draft_year = Column(Integer)
-    current_salary = Column(Integer)
-    hof_induction_year = Column(Integer)
-
-    def __str__(self):
-        output = ''
-        for c in self.__table__.columns:
-            output += '{}: {}\n'.format(c.name, getattr(self, c.name))
-        return output
-
-
-Base.metadata.create_all(engine)
-
-profiles_df = pd.read_json(PROFILES_JSON)
-profiles_df.set_index('player_id', inplace=True)
-profiles_df.to_sql(name=PROFILES_TABLE_NAME, con=engine, if_exists='replace')
-
-
-
-
-
+    db_converter = NFLDatabaseConverter(DATABASE_FILE)
+    db_converter.create_table_from_json(name=PROFILES_TABLE_NAME, json_file=PROFILES_JSON)
 
